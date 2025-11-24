c33f7c49f5ae92fb775dadfb1520fa89aa660a19
Zak Dyer <30128282@tafe.wa.edu.au>
2025-11-25 02:56:54 +0800

    Added empty MainViewModel
    WIP

---
+++ IDSA/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icarus_Drone_Service_Application.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public DroneFormViewModel? ActiveDroneForm = null;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
