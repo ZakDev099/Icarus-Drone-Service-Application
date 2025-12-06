6b6b1d2b5b544d87a7533006113c81d35e6aa06f
Zak Dyer <30128282@tafe.wa.edu.au>
2025-12-06 17:47:53 +0800

    changed: Drone
    Added service fee and custom constructor to drone to automatically apply fee to service cost.

--- IDSA/Models/Drone.cs
+++ IDSA/Models/Drone.cs
@@ -8,20 +8,48 @@ using System.Windows.Media.Animation;
 
 namespace Icarus_Drone_Service_Application.Models
 {
-    public class Drone(int serviceTag, string clientName, string droneModel, string serviceProblem, double serviceCost)
+    // 6.1 :: No setters were needed as the object is not required to be mutable or expected to change after construction
+    public class Drone
     {
-        private readonly int serviceTag = serviceTag;
+        private readonly int serviceTag;
         public int ServiceTag => serviceTag;
 
-        private readonly string clientName = clientName;
+        private readonly string clientName;
         public string ClientName => Utils.ToTitleCase(clientName);
 
-        private readonly string droneModel = droneModel;
+        private readonly string droneModel;
         public string DroneModel => droneModel;
 
-        private readonly string serviceProblem = serviceProblem;
+        private readonly string serviceProblem;
         public string ServiceProblem => Utils.ToSentenceCase(serviceProblem);
-        private readonly double serviceCost = serviceCost;
+
+        private readonly double serviceCost;
         public double ServiceCost => serviceCost;
+        private const double expressServiceFee = 0.15;
+
+        public Drone(int serviceTag, string clientName, string droneModel, string serviceProblem, double serviceCost, bool applyFee = false)
+        {
+            this.serviceTag = serviceTag;
+            this.clientName = clientName;
+            this.droneModel = droneModel;
+            this.droneModel = serviceProblem;
+            this.serviceProblem = serviceProblem;
+
+            if (applyFee)
+            {
+                this.serviceCost = serviceCost * (1 + expressServiceFee);
+            }
+            else
+            {
+                this.serviceCost = serviceCost;
+            }
+        }
+
+
+        // 6.1 ::
+        public (string, double) DisplayClientNameAndServiceCost()
+        {
+            return (ClientName, ServiceCost);
+        }
     }
 }
